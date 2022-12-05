1aa0037997f340a19ef34f2b261135deee783e87
old_urchin <drtibibo@yandrx.ru>
2022-12-06 00:30:13 +0300

    add files HomeWork Sem1

---
+++ Sem1/Dz_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Узнаем наибольшее из двух чисел!");
+Console.WriteLine("Введите первое число а:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число b:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Эти числа равны!!!");
+}
+else
+{
+    if (a > b)
+    {
+        Console.WriteLine($"Число a = {a}, число b = {b}");
+        Console.WriteLine($"{a} > {b}");
+        Console.WriteLine("a > b");
+        Console.WriteLine($"Max a = {a}");
+    }
+    else
+    {
+        Console.WriteLine($"Число a = {a}, число b = {b}");
+        Console.WriteLine($"{b} > {a}");
+        Console.WriteLine("b > a");
+        Console.WriteLine($"Max b = {b}");
+    }
+
+}
+
+
---
+++ Sem1/Dz_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Узнаем наибольшее из трех чисел!");
+Console.WriteLine("Введите первое число а:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число b:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите третье число с:");
+double v = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine($"Числа a={a}, b={b}, v={v}");
+
+double max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (v > max)
+{
+    max = v;
+}
+
+Console.WriteLine($"Максимальное число: {max}");
