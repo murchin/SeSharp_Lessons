a3d435e4933efc2e212aac31f772ed96d262677e
old_urchin <drtibibo@yandrx.ru>
2022-12-08 02:37:32 +0300

    добавлены задания к семинару 2

---
+++ Sem2/S2Dz_2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.WriteLine("Показываем третью цифру заданного числа! ");
+Console.WriteLine("Для чисел меньше нуля тоже!");
+Console.WriteLine("Невероятное зрелище!");
+Console.WriteLine("Будьте смелыми, введите число:");
+
+//Переводим в целые числа, чтобы избежать игр со string, chr и null.
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > -100 & n < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    if (n<0){n=-n;}
+    string result = Convert.ToString(n);
+    Console.WriteLine($"Третья цифра {result[2]}");
+}
+
